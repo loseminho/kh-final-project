0fb34c3c49c5021602375ca20f692105684bc7fd
danashinnn <103996283+danashinnn@users.noreply.github.com>
2022-10-26 14:04:18 +0900

    - 마이페이지 화면 구현

---
+++ src/main/webapp/resources/js/member/mypage.js
@@ -0,0 +1,24 @@
+$(".sub-menu").prev().append("<span class='more down'><i class='fa-solid fa-caret-down'></i></span>");
+$(".more").on("click", function(event){
+    if($(this).hasClass("down")) {
+        $(this).removeClass("down");
+        $(this).addClass("up");
+        $(this).html("<i class='fa-solid fa-caret-up'></i>");
+    } else if($(this).hasClass("up")) {
+        $(this).removeClass("up");
+        $(this).addClass("down");
+        $(this).html("<i class='fa-solid fa-caret-down'></i>");
+    }
+    $(this).parent().next().slideToggle();
+    event.stopPropagation();
+});
+
+$(".more").parent().on("click", function(){
+    $(this).children().click();
+});
+
+$(".section").on("click", function(){
+    $("#rowSession2").children().hide();
+    const idx = $(".section").index(this);
+    $("#rowSession2").children().eq(idx).show();
+});
