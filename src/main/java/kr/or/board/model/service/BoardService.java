5bd4dfffefa0376e041b52b3960e26889884cf1b
danashinnn <103996283+danashinnn@users.noreply.github.com>
2022-10-28 17:20:54 +0900

    - pull 땡겼는데 commit 하라고 생겼어요.. 뭐지?

--- src/main/java/kr/or/board/model/service/BoardService.java
+++ src/main/java/kr/or/board/model/service/BoardService.java
@@ -7,36 +7,14 @@ import org.springframework.stereotype.Service;
 
 import kr.or.board.model.dao.BoardDao;
 import kr.or.board.model.vo.QnaBoard;
-import kr.or.board.model.vo.QnaFile;
 
 @Service
 public class BoardService {
 	@Autowired
 	private BoardDao dao;
-	
-	//문의게시판 list
+
 	public ArrayList<QnaBoard> allQnaBoard() {
 		return dao.allQnaBoard();
 	}
-	
-
-	//문의게시판 insert(file,qna)
-	public int insertQnaBoard(QnaBoard q) {
-		System.out.println("dao 전 :" +q.getQnaNo());
-		int result = dao.insertQnaBoard(q);
-		System.out.println("dao 전 :" +q.getQnaNo());
-		if(result>0) {
-			//insert 된 qnaboard no 조회 
-			int boardNo = dao.selectQnaBoardNo();
-			//file_tbl insert 
-			for(QnaFile qf : q.getFileList()) {
-				qf.setBoardNo(boardNo);
-				result += dao.insertFile(qf);
-			}
-		}
-		return result;
-	}
-
-
 
 }
