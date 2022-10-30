94519ea63689f08fc71271d9799ca74e1b06adf1
danashinnn <103996283+danashinnn@users.noreply.github.com>
2022-10-31 02:33:56 +0900

    - 반려견 상세 정보 모달창 구현

---
+++ src/main/java/kr/or/dog/model/service/DogService.java
@@ -0,0 +1,25 @@
+package kr.or.dog.model.service;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.dog.model.dao.DogDao;
+import kr.or.dog.model.vo.Dog;
+
+@Service
+public class DogService {
+
+	@Autowired
+	private DogDao dao;
+
+	public ArrayList<Dog> selectMyDogList(int memberNo) {
+		return dao.selectMyDogList(memberNo);
+	}
+
+	public Dog SelectMyOneDog(int dogNo) {
+		return dao.selectMyOneDog(dogNo);
+	}
+	
+}
