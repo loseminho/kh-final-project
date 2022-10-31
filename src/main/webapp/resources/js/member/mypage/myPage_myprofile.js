af89d6a62e129443416aa29b792fa1631d9cab71
danashinnn <103996283+danashinnn@users.noreply.github.com>
2022-10-31 22:22:21 +0900

    Revert "Revert "Revert "Revert "- 추가 커밋""""
    This reverts commit a5fd0b82020add9e09d9086cd6637e975b7cd81f.

--- src/main/webapp/resources/js/member/mypage/myPage_myprofile.js
+++ src/main/webapp/resources/js/member/mypage/myPage_myprofile.js
@@ -187,6 +187,7 @@ $("#updateBtn").on("click", function(){
 	            cancelButtonText: '취소'
 	        }).then((result) => {
 	            if (result.isConfirmed) {
+	            	$.cookie("tab", 0);
 					verifyChk.prev().attr("name", "memberPhone");
 					$("#updateMemberForm").submit();
 	            }
