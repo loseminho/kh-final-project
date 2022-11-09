1536efc6e1b395cf32e31ca325c98cf54e986f1f
LAPTOP-DSAHINK9\jinyh <loseminho@gmail.com>
2022-11-09 14:58:47 +0900

    머지 충돌 후 변경 사항

--- src/main/java/kr/or/walk/controller/WalkController.java
+++ src/main/java/kr/or/walk/controller/WalkController.java
@@ -26,9 +26,9 @@ public class WalkController {
 	// WalkMate 모든 게시물 띄우기..
 	// WalkMate 모든 게시물 참여하는 유저 프로필
 	@ResponseBody
-	@RequestMapping(value="/allWalkListAjax.do", produces="application/json;charset=utf-8")
-	public String allWalkListAjax() {
-		ArrayList<Walk> list = service.allWalkList();
+	@RequestMapping(value="/selectContentBox.do", produces="application/json;charset=utf-8")
+	public String selectContentBox() {
+		ArrayList<Walk> list = service.selectContentBox();
 		return new Gson().toJson(list);
 	}
 }
