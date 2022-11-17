23ac59c97a40abb81b2168264d9b51601ef98236
danashinnn <103996283+danashinnn@users.noreply.github.com>
2022-11-17 14:54:03 +0900

    - 마이페이지 달력의 일정 클릭하면 해당 모임 정보 페이지로 넘어가도록 구현

--- src/main/webapp/resources/js/member/mypage/myPage_mycalendar.js
+++ src/main/webapp/resources/js/member/mypage/myPage_mycalendar.js
@@ -10,11 +10,9 @@ function mycalendar(){
 		titleFormat : function(date) {
 			return date.date.year + '년 ' + (parseInt(date.date.month) + 1) + '월';
 		},
-		// initialDate: '2021-07-15', // 초기 날짜 설정 (설정하지 않으면 오늘 날짜가 보인다.)
-		// eventLimit: true,
-		// selectable : true, // 달력 일자 드래그 설정가능
-		// droppable : true,
-		// editable : true,
+		dayMaxEventRows: true, // 달력에 셀 크기보다 많은 이벤트가 등록된 경우 more로 표시
+        editable: false,
+        droppable: false,
 		nowIndicator: true, // 현재 시간 마크
 		locale: 'ko', // 한국어 설정
 		events:function(info, successCallback, failureCallback){
@@ -25,29 +23,37 @@ function mycalendar(){
                    var events = [];
                    if(result != "null"){
 	                   for(let i=0; i<result.length; i++) {
-		
 	                        events.push({
 	                           title: result[i].wmTitle,
 	                           start: result[i].wmMeetTime,
 	                           end: result[i].wmMeetTime,
+	                           url: '/walkMatePage.do?wmNo='+result[i].wmNo,
 	                           description: result[i].wmAddr,
 	                           color : '#' + Math.round(Math.random() * 0xffffff).toString(16)                                           
 	                        }); // push() 끝
-	                        
 	                   } // for문 끝
-                       //console.log(events);
-                   }//if문 끝                          
+	                   
+                       // console.log(events);
+                   }// if문 끝              
+                               
                    successCallback(events);                               
-               }//success 끝                         
-        	}); //ajax 끝
-        }, //events 끝
-        
+               }// success 끝      
+                                  
+        	}); // ajax 끝
+        	
+        }, // events 끝
 		eventDidMount: function(info) {
             tippy(info.el, {
                 content: info.event.title +"<br>장소 : "+ info.event.extendedProps.description, //이벤트 타이틀을 툴팁으로 가져오기 
                 allowHTML: true
            });
-        }
+        },
+        eventClick: function(event) { // 달력 일정 클릭하면 모임 정보 페이지로 넘어가도록
+		    if (event.url) {
+		        window.open(event.url, "_blank");
+		        return false;
+		    }
+		}
 	});
 	
 	calendar.render(); // 달력 불러옴
